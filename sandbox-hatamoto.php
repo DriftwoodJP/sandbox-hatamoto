61c648b1c4ea7002d496d4a2923548c03881143e
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2017-07-27 10:27:16 +0900

    Add: wp scaffold plugin

---
+++ sandbox-hatamoto.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Sandbox Hatamoto
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     sandbox-hatamoto
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Sandbox_Hatamoto
+ */
